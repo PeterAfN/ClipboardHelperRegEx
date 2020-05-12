cc1e97411d3b7a66b7c9d87f92fd2376247a6d40
PeterAfN <64004978+PeterAfN@users.noreply.github.com>
2020-05-12 14:23:21 +0200

    37
    removed clipboard access denied warning message.

--- ClipboardHelper/BusinessLogic/SetTextClipboard.cs
+++ ClipboardHelper/BusinessLogic/SetTextClipboard.cs
@@ -25,12 +25,12 @@ namespace ClipboardHelperRegEx.BusinessLogic
                 System.Windows.Forms.Clipboard.SetDataObject(
                     text, // Text to store in clipboard
                     true, // Do not keep after our application exits
-                    5, // Retry 5 times
+                    1, 
                     100); // 100 ms delay between retries
             }
             catch (Exception e)
             {
-                MessageBox.Show(e + "\r\n\r\nClipboard Helper RegEx could not access the Clipboard. The program will try to again.");
+                //MessageBox.Show(e + "\r\n\r\nClipboard Helper RegEx could not access the Clipboard. The program will try to again.");
                 try
                 {
                     NativeMethods.CloseClipboard();
